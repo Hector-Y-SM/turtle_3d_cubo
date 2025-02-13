440bf5b49b88026cd69e3d7bc10f512bdb9276e5
Hector-Y-SM <hm96012@gmail.com>
2025-02-13 13:50:24 -0800

    codigo que dibuja lineas horizontales, verticales e inclinadas usando coordenadas

--- draw_line.py
+++ draw_line.py
@@ -1,20 +1,22 @@
-from turtle import *
-import turtle as t
-from tkinter import *
+import turtle
 
-t.hideturtle()  #no turtugaaaaaaaaaaaaaaaaaaa
-pu()
-
-
-def draw_line(steps):
-    for x in range(steps):
-        t.dot(3)
-        goto(x, x + 2)
-        #t.right(90)
-        t.dot(3)  
-
-
-draw_line(100)
-
-
+def dibujar_linea(x1, y1, x2, y2, pasos=100, tamaño=1):
+    for i in range(pasos + 1):
+        t = i / pasos
+        x = x1 + (x2 - x1) * t
+        y = y1 + (y2 - y1) * t
+        
+        print('x: ', x)
+        print('y: ', y)
+        
+        turtle.penup()
+        turtle.goto(x, y)
+        turtle.dot(tamaño)
+        
+turtle.speed(5000)
+dibujar_linea(0, -100, 0, 100)
+dibujar_linea(-100, -100, -100, 100)
+dibujar_linea(-100, 100, 0, 100)
+dibujar_linea(0, 100, -100, -100)
+dibujar_linea(-100, -100, 0, -100)
 input('')
